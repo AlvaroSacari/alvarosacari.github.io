0a03035e9ea0205dd73c414acc5249ef36e2d497
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-11-08 23:24:43 -0500

    contact component has been updated

--- components/index_sections/Contact.vue
+++ components/index_sections/Contact.vue
@@ -11,19 +11,76 @@
 
     <div class="row">
       <v-col>
-        <p>alvaro.sacari@gmail.com</p>
+        <v-btn color="primary" text class="text-lowercase" :href="`mailto:${email}?subject=Contacto`">
+          <v-icon left>
+            mdi-email-outline
+          </v-icon>
+          {{ email }}
+        </v-btn>
+        <br>
+        <v-btn color="primary" text class="text-capitalize">
+          <v-icon left>
+            mdi-map-marker
+          </v-icon>
+          {{ address }}
+        </v-btn>
+      </v-col>
+      <v-col>
+        <v-btn color="primary" text class="text-capitalize" target="_blank" :href="linkedIn">
+          <v-icon left>
+            mdi-linkedin-box
+          </v-icon>
+          LinkedIn
+        </v-btn>
+        <br>
+        <v-btn color="primary" text class="text-capitalize" target="_blank" :href="facebook">
+          <v-icon left>
+            mdi-facebook-box
+          </v-icon>
+          Facebook
+        </v-btn>
+      </v-col>
+      <v-col>
+        <v-btn color="primary" text class="text-capitalize" target="_blank" :href="github">
+          <v-icon left>
+            mdi-github-circle
+          </v-icon>
+          Github
+        </v-btn>
+        <br>
+        <v-btn color="primary" text class="text-capitalize" target="_blank" :href="gitlab">
+          <v-icon left>
+            mdi-gitlab
+          </v-icon>
+          Gitlab
+        </v-btn>
       </v-col>
     </div>
   </v-container>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data () {
     return {
 
     }
+  },
+
+  computed: {
+    ...mapState({
+      name: state => state.profile.name,
+      email: state => state.profile.email,
+      linkedIn: state => state.profile.linkedIn,
+      facebook: state => state.profile.facebook,
+      address: state => state.profile.address,
+      github: state => state.profile.github,
+      gitlab: state => state.profile.gitlab
+    })
   }
+
 }
 </script>
 
--- store/profile.js
+++ store/profile.js
@@ -2,7 +2,10 @@ export const state = () => ({
   name: 'Alvaro Sacari',
   email: 'alvaro.sacari@gmail.com',
   facebook: 'https://www.facebook.com/alvarosacari',
-  linkedIn: 'https://www.linkedin.com/in/alvaro-sacari'
+  linkedIn: 'https://www.linkedin.com/in/alvaro-sacari',
+  address: 'Tacna, Perú',
+  github: 'https://github.com/alvarosacari',
+  gitlab: 'https://gitlab.com/AlvaroS'
 })
 
 export const actions = {
