35b1ae78b1b7c251a83828633b4c1c30f67b0975
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-11-08 23:35:57 -0500

    links in app bar has been updated

--- components/default_layout/AppBar.vue
+++ components/default_layout/AppBar.vue
@@ -9,14 +9,11 @@
     <v-app-bar-nav-icon @click="toggleNavigationDrawer" />
     <v-toolbar-title>{{ title }}</v-toolbar-title>
     <v-spacer />
-    <v-btn icon>
+    <v-btn icon target="_blank" :href="linkedIn">
       <v-icon>mdi-linkedin-box</v-icon>
     </v-btn>
-    <v-btn icon>
-      <v-icon>mdi-gitlab</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>mdi-dots-vertical</v-icon>
+    <v-btn icon target="_blank" :href="github">
+      <v-icon>mdi-github-circle</v-icon>
     </v-btn>
   </v-app-bar>
 </template>
@@ -28,7 +25,9 @@ export default {
   computed: {
     ...mapState({
       clipped: state => state.navigationDrawer.clipped,
-      title: state => state.profile.name
+      title: state => state.profile.name,
+      linkedIn: state => state.profile.linkedIn,
+      github: state => state.profile.github
     })
   },
 
