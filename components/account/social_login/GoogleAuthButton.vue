04f8d3a9c38c706a6af5f851455178c4b1580b39
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-11-13 18:39:24 -0500

    the login with google auth has been advanced

---
+++ components/account/social_login/GoogleAuthButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-btn block color="#e34134" dark @click="loginWithGoogle">
+    <v-icon left>
+      mdi-google
+    </v-icon>
+    Ingresar con Google
+  </v-btn>
+</template>
+
+<script>
+import { auth, GoogleProvider } from '@/services/fireinit.js'
+
+export default {
+  data () {
+    return {
+      error: null
+    }
+  },
+  methods: {
+    loginWithGoogle () {
+      auth.signInWithRedirect(GoogleProvider)
+        .catch((error) => {
+          this.error = error
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
