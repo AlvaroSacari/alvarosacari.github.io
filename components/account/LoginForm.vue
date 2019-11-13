0e578ef562d024a1fdda0fbcf37f9fefb6eed2fe
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-11-13 18:38:30 -0500

    the login has been improved

---
+++ components/account/LoginForm.vue
@@ -0,0 +1,138 @@
+<template>
+  <div>
+    <GoogleAuthButton class="mb-4" />
+    <v-btn block class="mb-4" color="#1873eb" disabled>
+      <v-icon left>
+        mdi-facebook
+      </v-icon>
+      Ingresar con Facebook
+    </v-btn>
+
+    <v-fade-transition>
+      <v-alert
+        v-if="formError"
+        prominent
+        type="error"
+        border="left"
+      >
+        El usuario y/o la contraseña pueden ser incorrectos.
+      </v-alert>
+    </v-fade-transition>
+
+    <v-form
+      id="loginForm"
+      ref="loginForm"
+      v-model="validForm"
+      lazy-validation
+      @submit.prevent="login"
+    >
+      <v-text-field
+        v-model="email"
+        type="email"
+        outlined
+        label="Correo electrónico"
+        :rules="rules.email"
+      />
+      <v-text-field
+        v-model="password"
+        type="password"
+        outlined
+        label="Contraseña"
+        :rules="rules.password"
+      />
+    </v-form>
+
+    <v-btn
+      type="submit"
+      class="mb-4"
+      block
+      form="loginForm"
+      color="primary"
+      :loading="processingForm"
+      :disabled="!validForm || processingForm"
+    >
+      Ingresar
+    </v-btn>
+    <v-btn
+      text
+      color="primary"
+      block
+      :to="{ name: 'register' }"
+    >
+      Registrarme
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import GoogleAuthButton from '@/components/account/social_login/GoogleAuthButton.vue'
+
+export default {
+  components: {
+    GoogleAuthButton
+  },
+
+  data () {
+    return {
+      email: '',
+      password: '',
+      processingForm: false,
+      validForm: true,
+      formError: false,
+      rules: {
+        email: [
+          v => !!v || 'El correo electrónico es requerido',
+          v => /.+@.+\..+/.test(v) || 'El correo electrónico debe ser válido'
+        ],
+        password: [
+          v => !!v || 'La contraseña es requerida'
+        ]
+      }
+    }
+  },
+
+  watch: {
+    email (newValue, oldValue) {
+      if (newValue && newValue !== oldValue) {
+        this.formError = false
+      }
+    },
+
+    password (newValue, oldValue) {
+      if (newValue && newValue !== oldValue) {
+        this.formError = false
+      }
+    }
+  },
+
+  methods: {
+    login () {
+      if (!this.$refs.loginForm.validate()) { return }
+
+      this.processingForm = true
+      this.$emit('update-processing-form', true)
+
+      const data = {
+        email: this.email,
+        password: this.password
+      }
+
+      this.$auth.loginWith('firebaseAuth', { data })
+        .then((response) => {
+          this.$router.push({ name: 'dashboard' })
+        })
+        .catch(() => {
+          this.formError = true
+        })
+        .finally(() => {
+          this.processingForm = false
+          this.$emit('update-processing-form', false)
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,77 +1,13 @@
 <template>
   <v-container>
     <v-col>
-      <v-card max-width="300" class="mx-auto" :loading="processingForm">
+      <v-card max-width="300" class="mx-auto" :loading="loadingCard">
         <v-app-bar flat>
           <v-toolbar-title>Iniciar sesión</v-toolbar-title>
         </v-app-bar>
         <v-divider />
         <v-card-text>
-          <v-btn block class="mb-4" color="#e34134" dark>
-            <v-icon left>
-              mdi-google
-            </v-icon>
-            Ingresar con Google
-          </v-btn>
-          <v-btn block color="#1873eb" disabled>
-            <v-icon left>
-              mdi-facebook
-            </v-icon>
-            Ingresar con Facebook
-          </v-btn>
-        </v-card-text>
-        <v-card-text>
-          <v-fade-transition>
-            <v-alert
-              v-if="loginError"
-              prominent
-              type="error"
-              border="left"
-            >
-              El usuario y/o la contraseña pueden ser incorrectos.
-            </v-alert>
-          </v-fade-transition>
-          <v-form
-            id="loginForm"
-            ref="loginForm"
-            v-model="validForm"
-            lazy-validation
-            @submit.prevent="login"
-          >
-            <v-text-field
-              v-model="email"
-              type="email"
-              outlined
-              label="Correo electrónico"
-              :rules="rules.email"
-            />
-            <v-text-field
-              v-model="password"
-              type="password"
-              outlined
-              label="Contraseña"
-              :rules="rules.password"
-            />
-          </v-form>
-          <v-btn
-            type="submit"
-            class="mb-4"
-            block
-            form="loginForm"
-            color="primary"
-            :loading="processingForm"
-            :disabled="!validForm || processingForm"
-          >
-            Ingresar
-          </v-btn>
-          <v-btn
-            text
-            color="primary"
-            block
-            :to="{ name: 'register' }"
-          >
-            Registrarme
-          </v-btn>
+          <LoginForm @update-processing-form="updateLoadingCard" />
         </v-card-text>
       </v-card>
     </v-col>
@@ -79,57 +15,22 @@
 </template>
 
 <script>
+import LoginForm from '@/components/account/LoginForm.vue'
+
 export default {
-  data () {
-    return {
-      email: '',
-      password: '',
-      processingForm: false,
-      validForm: true,
-      rules: {
-        email: [
-          v => !!v || 'El correo electrónico es requerido',
-          v => /.+@.+\..+/.test(v) || 'El correo electrónico debe ser válido'
-        ],
-        password: [
-          v => !!v || 'La contraseña es requerida'
-        ]
-      },
-      loginError: false
-    }
+  components: {
+    LoginForm
   },
 
-  watch: {
-    email (newValue, oldValue) {
-      if (newValue && newValue !== oldValue) {
-        this.loginError = false
-      }
-    },
-    password (newValue, oldValue) {
-      if (newValue && newValue !== oldValue) {
-        this.loginError = false
-      }
+  data () {
+    return {
+      loadingCard: false
     }
   },
 
   methods: {
-    login () {
-      if (!this.$refs.loginForm.validate()) { return }
-
-      this.processingForm = true
-
-      const data = {
-        email: this.email,
-        password: this.password
-      }
-
-      this.$auth.loginWith('firebaseAuth', { data })
-        .catch(() => {
-          this.loginError = true
-        })
-        .finally(() => {
-          this.processingForm = false
-        })
+    updateLoadingCard (value) {
+      this.loadingCard = value
     }
   }
 }
