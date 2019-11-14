85b98efe2374c4249f1016c499b29a4c8a3caa6b
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-11-14 12:41:46 -0500

    router scroll behavior has been updated

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,7 +1,4 @@
 import goTo from 'vuetify/lib/services/goto'
-import { Breakpoint } from 'vuetify/lib/services/breakpoint'
-
-const breakpoint = new Breakpoint()
 
 export default function (to, from, savedPosition) {
   let scrollTo = 0
@@ -13,10 +10,6 @@ export default function (to, from, savedPosition) {
   }
 
   const options = { offset: 0 }
-
-  if (process.env.NODE_ENV !== 'production') {
-    options.offset = breakpoint.smAndDown ? 56 : 64
-  }
 
   goTo(scrollTo, options)
 }
