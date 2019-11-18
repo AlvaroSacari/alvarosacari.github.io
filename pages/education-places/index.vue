34e1f61d7c52687b35a1ef6011345814024c86f9
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2019-11-18 18:34:44 -0500

    title and v-card has been wrapped on div#education-places-table

--- pages/education-places/index.vue
+++ pages/education-places/index.vue
@@ -2,65 +2,67 @@
   <v-container>
     <Breadcrumbs :items="breadcrumbs" />
 
-    <h2 id="education-places" class="py-4">
-      <v-btn @click="$vuetify.goTo('#education-places')" icon color="primary">
-        <v-icon>
-          mdi-pound
-        </v-icon>
-      </v-btn>
-      Educación
-    </h2>
+    <div id="education-places-table">
+      <h2 class="py-4">
+        <v-btn @click="$vuetify.goTo('#education-places-table')" icon color="primary">
+          <v-icon>
+            mdi-pound
+          </v-icon>
+        </v-btn>
+        Educación
+      </h2>
 
-    <v-card>
-      <v-app-bar flat>
-        <v-text-field
-          v-model="search"
-          dense
-          outlined
-          placeholder="Buscar"
-          hide-details
-          prepend-inner-icon="mdi-magnify"
-          clearable
-        />
-        <v-btn color="primary" class="mx-2 text-capitalize">
-          Agregar
-        </v-btn>
-        <v-btn height="40" width="40" icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </v-app-bar>
-      <v-data-table
-        :headers="headers"
-        :items="educationPlaces"
-        :loading="loadingData"
-        :search="search"
-      >
-        <template v-slot:item.action="{ item }">
-          <div class="mx-n3">
-            <v-tooltip bottom>
-              <template v-slot:activator="{ on }">
-                <v-btn v-on="on" @click="editItem(item)" color="info" icon>
-                  <v-icon>
-                    mdi-pencil
-                  </v-icon>
-                </v-btn>
-              </template>
-              <span>Editar</span>
-            </v-tooltip>
-            <v-tooltip bottom>
-              <template v-slot:activator="{ on }">
-                <v-btn v-on="on" @click="editItem(item)" color="error" icon>
-                  <v-icon>
-                    mdi-delete
-                  </v-icon>
-                </v-btn>
-              </template>
-              <span>Eliminar</span>
-            </v-tooltip>
-          </div>
-        </template>
-      </v-data-table>
-    </v-card>
+      <v-card>
+        <v-app-bar flat>
+          <v-text-field
+            v-model="search"
+            dense
+            outlined
+            placeholder="Buscar"
+            hide-details
+            prepend-inner-icon="mdi-magnify"
+            clearable
+          />
+          <v-btn color="primary" class="mx-2 text-capitalize">
+            Agregar
+          </v-btn>
+          <v-btn height="40" width="40" icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+        <v-data-table
+          :headers="headers"
+          :items="educationPlaces"
+          :loading="loadingData"
+          :search="search"
+        >
+          <template v-slot:item.action="{ item }">
+            <div class="mx-n3">
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                  <v-btn v-on="on" @click="editItem(item)" color="info" icon>
+                    <v-icon>
+                      mdi-pencil
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Editar</span>
+              </v-tooltip>
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                  <v-btn v-on="on" @click="editItem(item)" color="error" icon>
+                    <v-icon>
+                      mdi-delete
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Eliminar</span>
+              </v-tooltip>
+            </div>
+          </template>
+        </v-data-table>
+      </v-card>
+    </div>
   </v-container>
 </template>
 
